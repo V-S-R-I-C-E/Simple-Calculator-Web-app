8b9da53a0dfda4ebe44bffb2fc921d58e7db11d1
V-S-R-I-C-E <vijayraj3951f@gmail.com>
2025-07-06 01:25:47 +0530

    debugged version

--- app.py
+++ app.py
@@ -8,15 +8,14 @@ def calculator () :
             num1 =float(request.form["num1"])
             num2 =float(request.form["num2"])
             operation = request.form["Operation"]
-               if operation == "Add" :
+            if operation == "Add" :
                 result =num1 + num2
-               elif operation == "Subtract" :
+            elif operation == "Subtract" :
                 result =num1 - num2
-               elif operation == "Multiply" :
+            elif operation == "Multiply" :
                 result =num1 * num2
-               elif operation == "Multiply" :
+            elif operation == "Multiply" :
                 result =num1 / num2 if num2 != 0 else "Error (Divide by 0)"
-           except ValueError : result ="Invalid input"
-        return
-    render_template("index.html",result = result)
+        except ValueError : result ="Invalid input"
+        return render_template("index.html",result = result)
     if __name__ == "__main__":app.run(debug= True)
