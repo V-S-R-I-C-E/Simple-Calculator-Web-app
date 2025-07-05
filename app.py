177160b168debf7ee5e1da5e9c14781bac386194
V-S-R-I-C-E <vijayraj3951f@gmail.com>
2025-07-05 09:05:21 +0530

    Intial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask as fl,render_template,request
+app=fl(__name__)
+@app.route("/",methods=["Get","POST"])
+def calculator () :
+    result = None
+    if request.method == "POST" :
+        try:
+            num1 =float(request.form["num1"])
+            num2 =float(request.form["num2"])
+            operation = request.form["Operation"]
+               if operation == "Add" :
+                result =num1 + num2
+               elif operation == "Subtract" :
+                result =num1 - num2
+               elif operation == "Multiply" :
+                result =num1 * num2
+               elif operation == "Multiply" :
+                result =num1 / num2 if num2 != 0 else "Error (Divide by 0)"
+           except ValueError : result ="Invalid input"
+        return
+    render_template("index.html",result = result)
+    if __name__ == "__main__":app.run(debug= True)
