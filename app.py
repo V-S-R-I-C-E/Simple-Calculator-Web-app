94fd35f09e87e412f1aa7978ba12fea03f2bfea8
V-S-R-I-C-E <vijayraj3951f@gmail.com>
2025-07-06 11:13:11 +0530

    modification

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session
 app=Flask(__name__)
+app.secret_key = "your_secret_key_here"
 @app.route("/",methods=["Get","POST"])
 def calculator () :
     result = None
     if request.method == "POST" :
-        try:
+        operation = request.form["Operation"]
+        round_places = request.form.get("round_places")
+        if operation =="Round":
+            try:
+                last_result=session.get("last_result")
+                if last_result is not None and round_places :
+                    last_result=float(last_result)
+                    round_places=int(round_places)
+                    result=round(last_result,round_places)
+                    session["last_result"]=result
+                else:           
+                    result = "Nothing to round." 
+            except (ValueError,TypeError)      :
+                result = "Invalid rounding input." 
+        else:
+         try:
             num1 =float(request.form["num1"])
-            num2 =float(request.form["num2"])
-            operation = request.form["Operation"]
+            num2 =float(request.form["num2"])                     
             if operation == "Add" :
                 result =num1 + num2
             elif operation == "Subtract" :
@@ -16,7 +31,11 @@ def calculator () :
                 result =num1 * num2
             elif operation == "Divide" :
                 result =num1 / num2 if num2 != 0 else "Error (Divide by 0)"
-        except ValueError :
+            elif operation == "Power" :
+                result =num1 ** num2 
+            if isinstance (result,(int,float)) :
+                session["last_result"]=result
+         except ValueError :
             result ="Invalid input"
     return render_template("index.html",result = result)
 if __name__ == "__main__":
